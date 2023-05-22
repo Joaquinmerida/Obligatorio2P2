038e07e7b5b2043782e8a750d71c0f628e0e7361
Joaquin Merida <joaquinmerida1997@gmail.com>
2023-05-22 16:34:22 -0300

    Mercado
    Constructor de lista mayoristas y puestos

--- SegundoObligatorioP2/src/segundoobligatoriop2/Sistema.java
+++ SegundoObligatorioP2/src/segundoobligatoriop2/Sistema.java
@@ -2,13 +2,18 @@
 package segundoobligatoriop2;
 
 import segundoobligatoriop2.interfaz.InterfazRegistro;
+import java.util.ArrayList;
 
 
 public class Sistema {
 
     public static void main(String[] args) {
+        
         InterfazRegistro v = new InterfazRegistro();
         v.setVisible(true);
+        
+        
+        
     }
 
 
