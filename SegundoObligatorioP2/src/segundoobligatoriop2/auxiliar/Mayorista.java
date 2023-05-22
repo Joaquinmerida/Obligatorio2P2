7f2b68ad88ea4b667589d1a0a5e89d013d86593e
Joaquin Merida <joaquinmerida1997@gmail.com>
2023-05-22 16:50:50 -0300

    -

--- SegundoObligatorioP2/src/segundoobligatoriop2/auxiliar/Mayorista.java
+++ SegundoObligatorioP2/src/segundoobligatoriop2/auxiliar/Mayorista.java
@@ -1,18 +1,20 @@
 package segundoobligatoriop2.auxiliar;
 
+import java.util.*;
+
 public class Mayorista {
+
     private String rut;
     private String nombre;
     private String direccion;
+    private ArrayList<Item> listaItems;
 
-    
     // falta la lista de productos que es array
-    
-    
-    public Mayorista(String rut, String nombre, String direccion) {
+    public Mayorista(String rut, String nombre, String direccion, ArrayList listaItems) {
         this.rut = rut;
         this.nombre = nombre;
         this.direccion = direccion;
+        this.listaItems = listaItems;
     }
 
     public String getRut() {
@@ -37,6 +39,14 @@ public class Mayorista {
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+    }
+
+    public ArrayList<Item> getListaItems() {
+        return listaItems;
+    }
+
+    public void agregarItem(Item unItem) {
+        this.getListaItems().add(unItem);
     }
 
 }
